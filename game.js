fb595f669d922ce09f022d1de207c3ee62b20ecf
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-11 13:23:37 +0100

    image highlighting

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+
+// We create an object that stores the current game.
+const game = {
+    playerHand: "",
+    aiHand: "",
+}
+const hands = [...document.querySelectorAll('.select img')]
+
+//First function
+function handSelection() {
+
+    game.playerHand = this.dataset.option;
+
+    console.log(game.playerHand);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0  4px yellow';
+}
+
+
+hands.forEach(hand => hand.addEventListener('click', handSelection));
