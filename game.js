2c870c300e86bd4e8a5958943a100cae7f668d5e
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-12 14:11:46 +0100

    Showing results

--- game.js
+++ game.js
@@ -42,6 +42,32 @@ function chceckResult(player, ai) {
 
 }
 
+//Publication of results
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    gameSummary.numbers++;
+    console.log(gameSummary.numbers);
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "Ty wygrałeś!!!!"
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "losse") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał :("
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis :\\"
+        document.querySelector('[data-summary="who-win"]').style.color = "gray";
+    }
+}
+
 //function stering
 function startGame() {
     if (!game.playerHand) {
@@ -50,6 +76,10 @@ function startGame() {
 
     game.aiHand = aiChoice();
     const gameResult = chceckResult(game.playerHand, game.aiHand);
+
+    console.log(gameResult);
+
+    publishResult(game.playerHand, game.aiHand, gameResult);
 
 }
 
